154fe11a51bc6f495c881a13e7f679e553195ff8
Massar <massarabbas62@gmail.com>
2023-05-04 11:30:35 +0200

    changed docker-compose.yml and added new spring project as config server

---
+++ config-serv/src/main/java/com/insa/configserv/ConfigServApplication.java
@@ -0,0 +1,13 @@
+package com.insa.configserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServApplication.class, args);
+	}
+
+}
